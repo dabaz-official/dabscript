c72158cdb1c7629873cfd43b73a861c50a654160
DabAZ <dabaz@dabaz.me>
2026-05-22 13:08:34 +0800

    feat(config): configure site URL, title, and branding

---
+++ app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  seo: {
+    titleTemplate: '%s - DabScript',
+    title: 'DabScript',
+    description: 'Learn JavaScript with DabScript.'
+  },
+  header: {
+    title: 'DabScript',
+    logo: {
+      light: '/logo-light.svg',
+      dark: '/logo-dark.svg'
+    }
+  }
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,7 @@
 export default defineNuxtConfig({
+  site: {
+    name: 'DabScript',
+    url: 'https://script.dabaz.me',
+  },
   extends: ['docus'],
 })
