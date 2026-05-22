3e9d36154ae7a93e1c52ab89549dc5e90b43e15b
DabAZ <dabaz@dabaz.me>
2026-05-22 14:07:53 +0800

    feat(config): configure GitHub integration

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   seo: {
     titleTemplate: '%s - DabScript',
     title: 'DabScript',
-    description: 'Learn JavaScript with DabScript.'
+    description: 'A no-nonsense, essential JavaScript guide for beginners, focusing on core concepts and clear, actionable syntax.'
   },
   header: {
     title: 'DabScript',
@@ -10,5 +10,10 @@ export default defineAppConfig({
       light: '/logo-light.svg',
       dark: '/logo-dark.svg'
     }
-  }
+  },
+  github: {
+    url: 'https://github.com/dabaz-official/dabscript',
+    branch: 'main',
+    rootDir: 'content',
+  },
 })
